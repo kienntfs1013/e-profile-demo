6cc4083cc9ee8550221703e4be2820e18e7d5413
Nguyen Tri Kien <trikienthachban@gmail.com>
2025-08-18 21:50:46 +0700

    fix(route): dynamic update/[id] (no dynamicParams)

--- src/app/dashboard/customers/(forms)/update/[id]/page.tsx
+++ src/app/dashboard/customers/(forms)/update/[id]/page.tsx
@@ -1,12 +1,5 @@
-import { users } from "@/models/user";
-
 import ClientPage from "./client-page";
 
-export async function generateStaticParams() {
-	return users.map((u) => ({ id: u.id }));
-}
-
 export default function Page({ params }: { params: { id: string } }) {
-	const { id } = params;
-	return <ClientPage id={id} />;
+	return <ClientPage id={params.id} />;
 }
