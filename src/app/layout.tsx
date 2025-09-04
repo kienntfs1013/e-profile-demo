0f9c5d925dfdb62cb441cdab63af89fd8173bd2f
Nguyen Tri Kien <trikienthachban@gmail.com>
2025-09-04 09:58:34 +0700

    fix: remove @import in global.css; import fonts via JS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import type { Viewport } from "next";
 
-import "@/styles/global.css";
-
 import { UserProvider } from "@/contexts/user-context";
 import { LocalizationProvider } from "@/components/core/localization-provider";
 import { ThemeProvider } from "@/components/core/theme-provider/theme-provider";
